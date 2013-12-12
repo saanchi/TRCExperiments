20f31a230e224e4d9746ebda08cb3ccd94b520fa
Sanchit Gupta <saanchithefriend@gmail.com>
2013-12-12 02:26:13 -0500

    added support for crawling ajax responses

--- python/experienceProject/spiders/experienceproject.py
+++ python/experienceProject/spiders/experienceproject.py
@@ -1,46 +1,46 @@
 from scrapy.selector import HtmlXPathSelector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from experienceProject.items import ExperienceprojectItem
-from scrapy.utils.url import urljoin_rfc
 from scrapy.http import Request
 from scrapy.http import Request, HtmlResponse, FormRequest
 import re
+import json
+import pdb
+import time
 
 class ExperienceprojectSpider(CrawlSpider):
     name = 'experienceproject'
-    start_urls = ['http://www.experienceproject.com/stories/topics/emotional-health','http://www.experienceproject.com/stories/topics/relationship-struggles','http://www.experienceproject.com/stories/topics/broken-hearts-and-betrayal','http://www.experienceproject.com/stories/topics/singledom','http://www.experienceproject.com/stories/topics/self-harm','http://www.experienceproject.com/stories/topics/phobias','http://www.experienceproject.com/stories/topics/drugs','http://www.experienceproject.com/stories/topics/regrets','http://www.experienceproject.com/stories/topics/addiction','http://www.experienceproject.com/stories/topics/loneliness','http://www.experienceproject.com/stories/topics/abuse','http://www.experienceproject.com/stories/topics/suicide','http://www.experienceproject.com/stories/topics/personality-disorder','http://www.experienceproject.com/stories/topics/bipolar-disorder','http://www.experienceproject.com/stories/topics/image-and-weight','http://www.experienceproject.com/stories/topics/memories','http://www.experienceproject.com/stories/topics/loss-of-a-pet','http://www.experienceproject.com/stories/topics/loss-of-a-child','http://www.experienceproject.com/stories/topics/loss-of-a-friend','http://www.experienceproject.com/stories/topics/loss-in-the-family','http://www.experienceproject.com/stories/topics/family-struggles','http://www.experienceproject.com/stories/topics/loss-to-suicide','http://www.experienceproject.com/stories/topics/pet-peeves','http://www.experienceproject.com/stories/topics/self-improvement','http://www.experienceproject.com/stories/topics/unemployment','http://www.experienceproject.com/stories/topics/ptsd']    
+    start_urls = ['http://www.experienceproject.com']
+    category_list = ['emotional-health','relationship-struggles','broken-hearts-and-betrayal','singledom','self-harm','phobias','drugs','regrets','addiction','loneliness','abuse','suicide','personality-disorder','bipolar-disorder','image-and-weight','memories','loss-of-a-pet','loss-of-a-child','loss-of-a-friend','loss-in-the-family','family-struggles','loss-to-suicide','pet-peeves','self-improvement','unemployment','ptsd']
+    category_count_map = {'emotional-health':1,'relationship-struggles':1,'broken-hearts-and-betrayal':1,'singledom':1,'self-harm':1,'phobias':1,'drugs':1,'regrets':1,'addiction':1,'loneliness':1,'abuse':1,'suicide':1,'personality-disorder':1,'bipolar-disorder':1,'image-and-weight':1,'memories':1,'loss-of-a-pet':1,'loss-of-a-child':1,'loss-of-a-friend':1,'loss-in-the-family':1,'family-struggles':1,'loss-to-suicide':1,'pet-peeves':1,'self-improvement':1,'unemployment':1,'ptsd':1} 
+    #category_list=['health']    
 
-    def __init__(self) :
-        response = FormRequest(url="http://www.experienceproject.com/dologin.php",
-                    formdata={'login_username': 'trcexperiment@gmail.com', 'login_password': 'trcexperiment123'})
-        print response
-
-     
+    def start_requests(self):
+        data = {'format':'json','isAjax':'true','limit':'20','order':'all'}
+        for category in self.category_list :
+            data['category'] = category
+            data['offset']   = str(self.category_count_map[category])
+            time.sleep(1)
+            yield FormRequest( url="http://www.experienceproject.com/ajax/category/get-more-stories-by-category.php", callback=self.parse, formdata=data) 
+ 
     def parse(self, response):
-        hxs = HtmlXPathSelector(response)
-        sites = hxs.xpath('//a[contains(@href, "stories")]/@href')          
-        url = ''
-        pattern = re.compile('stories/[a-zA-Z\-]*/[0-9]*')        
-        for site in sites:
-            relative_url = site.extract()
-            url = self._urljoin(response,relative_url)
-            if  pattern.match(url) and url not in start_urls :
-                print url
-                yield Request(url, callback = self.parsetext)
+        form_data = {'format':'json','isAjax':'true','limit':'20','order':'all'}
+        data = json.loads( response.body)
+        ## extract the category from url
+        if data['nextHref'] :
+            next_ref = data['nextHref']
+        if next_ref :        
+            split_list = data['nextHref'].split('/')
+            for story in data['stories']:
+                print "%s\t%s" % ( split_list[5].encode('utf-8'),  story['content'].encode('utf-8'))
+            ## Form the request again
+            if split_list[5] in self.category_list :
+                form_data['category'] = split_list[5].encode('ascii')
+                form_data['offset']   = str( self.category_count_map[split_list[5]]+1)
+                self.category_count_map[split_list[5]] += 1  
+                #print "**********************************************************"
+                #print form_data
+                #pdb.set_trace()
+                time.sleep(1) 
+                yield FormRequest( url="http://www.experienceproject.com/ajax/category/get-more-stories-by-category.php", callback=self.parse, formdata=form_data) 
 
-    def parsetext(self, response):
-        print url
-        log = open("log.txt", "a")
-        hxs = HtmlXPathSelector(response)
-        items = []
-        texts = hxs.xpath('//div[contains(@id,"story_div")]/text()').extract()
-        text = ''.join(str(e) for e in texts)
-        print text
-        log.write(text)
-        log.close()
-        #return items
-
-    def _urljoin(self, response, url):
-        """Helper to convert relative urls to absolute"""
-        return urljoin_rfc(response.url, url, response.encoding)
